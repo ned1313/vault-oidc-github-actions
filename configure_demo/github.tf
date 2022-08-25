d71356111bb6c92c9fce0e96a9dfe8257c70bc6c
ned1313 <ned.bellavance@gmail.com>
2022-08-25 09:34:37 -0400

    add secert path and key

--- configure_demo/github.tf
+++ configure_demo/github.tf
@@ -5,10 +5,11 @@ data "github_repository" "repo" {
 
 resource "github_actions_secret" "secrets" {
   for_each = {
-    "VAULT_ADDR"      = var.vault_server_url
-    "VAULT_NAMESPACE" = "admin"
-    "VAULT_SECRET"    = vault_kv_secret_v2.taco.path
-    "VAULT_ROLE"      = vault_jwt_auth_backend_role.example.role_name
+    "VAULT_ADDR"        = var.vault_server_url
+    "VAULT_NAMESPACE"   = "admin"
+    "VAULT_SECRET_PATH" = vault_kv_secret_v2.taco.path
+    "VAULT_SECRET_KEY"  = "pepper"
+    "VAULT_ROLE"        = vault_jwt_auth_backend_role.example.role_name
   }
   secret_name     = each.key
   plaintext_value = each.value
